2cc916534e75c4cd76d0532eca6b2985409eb7ae
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-10-21 21:01:26 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,127 @@
+const Discord = require("discord.js")
+
+const moment = require("moment")
+
+const client = new Discord.Client();
+
+const canvas = require("canvas");
+
+const fs = require('fs')
+
+const child_process = require("child_process");
+
+const Rocket = new Discord.Client();
+
+const jimp = require("jimp");// npm i jimp
+
+const package = ('package.json');
+
+const Util = require('discord.js');
+
+const getYoutubeID = require('get-youtube-id');
+
+const fetchVideoInfo = require('youtube-info');
+
+const YouTube = require('simple-youtube-api');
+
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+
+const queue = new Map();
+
+const ytdl = require('ytdl-core');
+
+const dateFormat = require('dateformat');
+
+
+
+
+
+
+
+const prefix = '$'
+
+
+///
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+
+
+client.on('guildMemberAdd', member=> {
+
+
+
+    client.channels.get("502466166095216641").send(`** 🐼 # Wèlčomè Ťo Panda Army.🐼  **
+**# أسـم العضُـو.: ${member} .
+  # أنت رُقـم.: ${member.guild.memberCount} . **`);
+
+    });
+
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "🐼 «⟨ PanDa ArMy ⟩» 🐼")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', '🌏➖𝐜𝐡𝐚𝐭➖🌏');
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "🐼 «⟨ PanDa ArMy ⟩» 🐼")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+ channel.send(`** By : # ${Invite.inviter} :white_sun_cloud: ** `)            
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
+});
+
+client.login(process.env.BOT_TOKEN);
